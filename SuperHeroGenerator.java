066e15143f5d32e4c3f22e2d3116e0ce5cecb1bb
Will Dalrymple <William.dalrymple@spartans.ut.edu>
2017-04-15 22:18:02 -0400

    Add files via upload

---
+++ SuperHeroGenerator.java
@@ -0,0 +1,14 @@
+/*ITM 251 - Section P - Spring 2017 - Dr.Maurer
+Topic: HW5
+Date: 4/15/17
+This File: SuperHeroGenerator
+Dependencies:  */
+
+public class SuperHeroGenerator { //opens driver class
+   
+   public static void main(String[] args) {
+   
+   
+   
+   }
+}
