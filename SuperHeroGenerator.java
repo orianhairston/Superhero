13d2576f3b5732edc166802a86c9c77667b083e7
Will Dalrymple <William.dalrymple@spartans.ut.edu>
2017-04-15 22:18:48 -0400

    Update SuperHeroGenerator.java

--- SuperHeroGenerator.java
+++ SuperHeroGenerator.java
@@ -4,11 +4,14 @@ Date: 4/15/17
 This File: SuperHeroGenerator
 Dependencies:  */
 
+import Java.util.Scanner;
+
 public class SuperHeroGenerator { //opens driver class
    
    public static void main(String[] args) {
    
    
+      
    
    }
 }
